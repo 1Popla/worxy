15fbe1f61af3ac607236d942cd7d28e1a4a29dc7
1Popla <marcin.poplawski988@gmail.com>
2024-05-16 12:55:06 +0200

    Fix user logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,9 @@ import "controllers"
 
 import "./message-scroll.js"
 
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+ActiveStorage.start()
