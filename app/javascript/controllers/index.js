84c16e99de0b868e44ac93a3b340837c4131e3c4
1Popla <marcin.poplawski988@gmail.com>
2024-06-10 09:40:52 +0200

    Move js code to separate file

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,10 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+// Register your posts controller manually if needed
+import PostsController from "./posts_controller"
+application.register("posts", PostsController)
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
