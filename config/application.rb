6f67b11f2910c38920219f20b77baf51c6da4b6b
1Popla <marcin.poplawski988@gmail.com>
2024-06-12 21:33:28 +0200

    Add i18n locales

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module BetterFixly
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    config.i18n.default_locale = :pl
+    config.i18n.available_locales = [:en, :pl]
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
