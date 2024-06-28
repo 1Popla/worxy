d21838a4c5d313a90af063a63c0f1bb37eddb6ca
1Popla <marcin.poplawski988@gmail.com>
2024-06-28 17:44:46 +0200

    Add dates in polish to calnendar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,9 +17,11 @@ class BookingsController < ApplicationController
   def calendar
     customer_or_visible_booking_ids = current_user.bookings.select(:id)
     visible_booking_ids = Booking.where(visible_to_user_id: current_user.id).select(:id)
-    worker_booking_ids = Booking.joins(:post).where(posts: {user_id: current_user.id}).select(:id)
+    worker_booking_ids = Booking.joins(:post).where(posts: { user_id: current_user.id }).select(:id)
 
     @bookings = Booking.where(id: customer_or_visible_booking_ids + worker_booking_ids + visible_booking_ids).distinct
+
+    @month = params[:month] ? Date.parse(params[:month]) : Date.current
   end
 
   def show
