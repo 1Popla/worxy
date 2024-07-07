e1da989fbea6688798dcd10e5760fe37888f5895
1Popla <marcin.poplawski988@gmail.com>
2024-07-07 15:26:40 +0200

    Change after singup path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(_resource)
-    dashboard_index_path
+    posts_path
   end
 
   def configure_permitted_parameters
