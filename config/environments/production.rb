26bfcf447e9973230f74d168f3db4b97ad6425a0
1Popla <marcin.poplawski988@gmail.com>
2024-08-11 23:34:37 +0200

    Update prod mail env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://worxy.onrender.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
