4d3d9730e798bd1831dd4fac3b14509cadc4ca39
1Popla <marcin.poplawski988@gmail.com>
2024-08-16 14:27:08 +0200

    Fix recipent and sender message sides

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,15 @@ class Message < ApplicationRecord
   validates :body, presence: { message: "Cannot send an empty message" }
 
   after_create_commit do
-    broadcast_append_later_to conversation, target: "messages", partial: "messages/message", locals: { message: self, current_user_id: user_id }
+    broadcast_append_later_to "#{conversation.id}_#{user_id}",
+                              target: "messages",
+                              partial: "messages/message",
+                              locals: { message: self, current_user_id: user_id }
+
+    recipient_id = conversation.sender_id == user_id ? conversation.recipient_id : conversation.sender_id
+    broadcast_append_later_to "#{conversation.id}_#{recipient_id}",
+                              target: "messages",
+                              partial: "messages/message",
+                              locals: { message: self, current_user_id: recipient_id }
   end
 end
