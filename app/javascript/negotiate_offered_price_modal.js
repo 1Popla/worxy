349cc89d1c2b7d0720be778e950e2f619fa0ddaa
1Popla <marcin.poplawski988@gmail.com>
2024-12-11 17:10:50 +0100

    Delete negotiatie price button

--- app/javascript/negotiate_offered_price_modal.js
+++ app/javascript/negotiate_offered_price_modal.js
@@ -1,17 +1,19 @@
-document.addEventListener('turbo:load', function() {
-  const openModalBtn = document.getElementById('negotiate-modal-open-btn');
-  const closeModalBtn = document.getElementById('negotiate-modal-close-btn');
-  const modal = document.getElementById('negotiate-modal');
+// BUTTON FOR PRICE NEGOTIATION
 
-  if (openModalBtn && closeModalBtn && modal) {
-    openModalBtn.addEventListener('click', function() {
-      modal.classList.remove('hidden');
-      modal.classList.add('flex');
-    });
+// document.addEventListener('turbo:load', function() {
+//   const openModalBtn = document.getElementById('negotiate-modal-open-btn');
+//   const closeModalBtn = document.getElementById('negotiate-modal-close-btn');
+//   const modal = document.getElementById('negotiate-modal');
 
-    closeModalBtn.addEventListener('click', function() {
-      modal.classList.remove('flex');
-      modal.classList.add('hidden');
-    });
-  }
-});
+//   if (openModalBtn && closeModalBtn && modal) {
+//     openModalBtn.addEventListener('click', function() {
+//       modal.classList.remove('hidden');
+//       modal.classList.add('flex');
+//     });
+
+//     closeModalBtn.addEventListener('click', function() {
+//       modal.classList.remove('flex');
+//       modal.classList.add('hidden');
+//     });
+//   }
+// });
