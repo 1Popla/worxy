ae1a51cb794098f8afa81e3608866a8a8fd007a9
1Popla <marcin.poplawski988@gmail.com>
2025-03-07 15:25:48 +0100

    Add more translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,32 +2,32 @@ module ApplicationHelper
   def human_readable_priority(priority)
     case priority
     when "no_priority"
-      "Brak"
+      I18n.t("helpers.priority.no_priority")
     when "high_priority"
-      "Wysoki"
+      I18n.t("helpers.priority.high_priority")
     when "medium_priority"
-      "Średni"
+      I18n.t("helpers.priority.medium_priority")
     when "low_priority"
-      "Niski"
+      I18n.t("helpers.priority.low_priority")
     else
-      "Nieznany"
+      I18n.t("helpers.priority.unknown")
     end
   end
 
   def human_enum_name(enum_name, enum_key)
     translations = {
-      "booking.statuses" => {
-        "pending" => "Oczekujący",
-        "confirmed" => "Potwierdzony",
-        "completed" => "Zakończony",
-        "cancelled" => "Anulowany"
+      "helpers.booking.statuses" => {
+        "pending" => I18n.t("helpers.booking.statuses.pending"),
+        "confirmed" => I18n.t("helpers.booking.statuses.confirmed"),
+        "completed" => I18n.t("helpers.booking.statuses.completed"),
+        "cancelled" => I18n.t("helpers.booking.statuses.cancelled")
       },
-      "user.roles" => {
-        "worker" => "Fachowiec",
-        "customer" => "Klient"
+      "helpers.user.roles" => {
+        "worker" => I18n.t("helpers.user.roles.worker"),
+        "customer" => I18n.t("helpers.user.roles.customer")
       }
     }
 
-    translations.dig(enum_name.to_s, enum_key.to_s) || enum_key.to_s.humanize
+    translations.dig("helpers.#{enum_name}", enum_key.to_s) || enum_key.to_s.humanize
   end
 end
